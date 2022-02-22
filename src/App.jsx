ca37cab929c4025d9effc776eccee4db71608b83
Allahnana <Usmanallahnana@gmail.com>
2022-02-22 21:01:40 +0100

    Hooks Doings

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import ClassCounter from './Components/ClassCounter'
+import HookCounter from './Components/HookCounter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        <ClassCounter/>
+        {/* <ClassCounter/> */}
+
+        <HookCounter/>
     </div>
   )
 }
