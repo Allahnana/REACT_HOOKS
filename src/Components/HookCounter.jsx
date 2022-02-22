6666186c464ad5d489c6c55f52bec1b830139a56
Allahnana <Usmanallahnana@gmail.com>
2022-02-22 21:30:54 +0100

    Hooks Doings

--- src/Components/HookCounter.jsx
+++ src/Components/HookCounter.jsx
@@ -5,7 +5,9 @@ function HookCounter() {
     const [count, setCount] = useState(0)
   return (
     <div>
-        <button onClick={() => setCount(count + 1)}>Count {count}</button>
+        <h1>My Very First Counter using HOOKS</h1>
+        <img  className="photo"src={('https://tsh.io/wp-content/uploads/fly-images/14921/react-hooks-best-practices-lead_-810x540.jpg')} />
+        <button className="btn"  onClick={() => setCount(count + 1)}>Count {count}</button>
     </div>
   )
 }
