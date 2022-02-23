0fe4fce32feab4391ebf54e2a92cec39b2829fc0
Allahnana <Usmanallahnana@gmail.com>
2022-02-23 16:35:33 +0100

    Doings

--- src/Components/HookCounter2.jsx
+++ src/Components/HookCounter2.jsx
@@ -5,13 +5,23 @@ function HookCounter2() {
     const initialCount = 0
 
     const [count, setCount] = useState(initialCount)
+
+    const increamentFive = () => {
+        for(let i =0; i<5; i++){
+
+            setCount (prevCount => prevCount + 1)
+
+        }
+       
+    }
   return (
     <div>
 
         Count : {count}
-        <button onClick={() => setCount(count + 1)}>Increament</button>
-        <button onClick={() => setCount(count - 1)}>Decreament</button>
+        <button onClick={() =>   setCount (prevCount => prevCount + 1)}>Increament</button>
+        <button onClick={() =>   setCount (prevCount => prevCount - 1)}>Decreament</button>
         <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={increamentFive}>Increament 5</button>
 
     </div>
   )
